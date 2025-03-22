c0a2f558a8c56632628acde5c6fea20fba5a49d6
Anand T <anandprince100@gmail.com>
2025-03-22 19:59:11 +0530

    blind 75 2 problems solved

---
+++ DataStructures/DSA/Blind 75/MaximumSubArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSA.Blind_75
+{
+    // Find a contigous sub array whose sum is largest and return sum
+    internal class MaximumSubArray
+    {
+        public static void Main(string[] args)
+        {
+            BruteForceMethod();
+
+
+        }
+
+        private static void BruteForceMethod()
+        {
+            int[] ipArray = [2, -1, 3, 4, -1, 2, 1, -5, 4];
+            int maxSum = 0;
+            List<int> opList = new List<int>();
+
+            for (int i = 0; i < ipArray.Length; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ DataStructures/DSA/Blind 75/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace DSA.Blind_75
+{
+    // The ouput array should be formed out of multiplying all other elements in the array except self
+    internal class ProductOfArrayExceptSelf
+    {
+        public static void Main(string[] args)
+        {
+            BruteForceMethod();
+
+            BestSolutionWithPrefixAndPostFix();
+
+        }
+
+        // Time Complexity : O(n), Space Complexity: O(n)
+        private static void BestSolutionWithPrefixAndPostFix()
+        {
+            int[] ipArr = [1, 2, 3, 4];
+            int[] opArr = new int[ipArr.Length];
+
+            int prefix = 1;
+            for (int i = 0; i < ipArr.Length; i++)
+            {
+                opArr[i] = prefix;
+                prefix = prefix * ipArr[i];
+            }
+
+            int postfix = 1;
+            for (int i = ipArr.Length - 1; i >= 0; i--)
+            {
+                opArr[i] = opArr[i] * postfix;
+                postfix = postfix * ipArr[i];
+            }
+
+            PrintOutputArrayElements(opArr, "BestSolutionWithPrefixAndPostFix");
+
+        }
+
+        // Time Complexity : O(n^2), Space Complexity: O(n)
+        private static void BruteForceMethod()
+        {
+            int[] ipArr = [1, 2, 3, 4];
+            int[] opArr = new int[ipArr.Length];
+            for (int i = 0; i < ipArr.Length; i++)
+            {
+                int multipliedNumber = 1;
+                for (int j = 0; j < ipArr.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        multipliedNumber = multipliedNumber * ipArr[j];
+                    }
+                }
+                opArr[i] = multipliedNumber;
+            }
+
+            PrintOutputArrayElements(opArr, "Brute Force");
+        }
+
+        private static void PrintOutputArrayElements(int[] opArr, string method)
+        {
+            Console.Write("o/p array for {0} = ", method);
+            foreach (int value in opArr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
